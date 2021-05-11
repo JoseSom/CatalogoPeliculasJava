370e9e0aec3ccdbe1a0f35745be2bb529dace834
Jose Somilleda <joseluissom004@gmail.com>
2021-05-11 00:33:14 -0500

    Implementacion de metodos en la interface y clase llamada CatalogoPeliculas

--- src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -1,13 +1,13 @@
 package cpjlaboratoriofinal;
 
 import java.util.Scanner;
-import mx.com.gm.peliculas.negocio.CatalogoPeliculas;
+import mx.com.gm.peliculas.negocio.ICatalogoPeliculas;
 
 public class CPJLaboratorioFinal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcion;
         String nombreArchivo;
-        CatalogoPeliculas catalogoPeliculas;
+        ICatalogoPeliculas catalogoPeliculas;
     }
 }
--- src/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
+++ src/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
@@ -1,27 +1,75 @@
 package mx.com.gm.peliculas.negocio;
 
-public class CatalogoPeliculasImpl implements CatalogoPeliculas{
+
+import java.util.List;
+import mx.com.gm.peliculas.datos.*;
+import mx.com.gm.peliculas.domain.*;
+import mx.com.gm.peliculas.excepciones.*;
+
+public class CatalogoPeliculasImpl implements ICatalogoPeliculas{
     
-    public CatalogoPeliculasImpl(){}
+    private final IAccesoDatos datos;
     
-    @Override
-    public void agregarPelicula(String nombrePelicula, String nombreArchivo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public CatalogoPeliculasImpl(){
+        this.datos = new AccesoDatosImpl();
     }
 
     @Override
-    public void listarPeliculas(String nombreArchivo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void agregarPelicula(String nombrePelicula) {
+        Pelicula pelicula = new Pelicula(nombrePelicula);
+        boolean anexar = false;
+        try {
+            anexar = datos.existe(NOMBRE_RECURSO);
+            datos.escribir(pelicula, NOMBRE_RECURSO, anexar);
+            System.out.println("Se ha agregrado correctamente");
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
-    public void buscarPelicula(String nombreArchivo, String buscar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void listarPeliculas() {
+        try {
+            List<Pelicula> peliculas = this.datos.listar(NOMBRE_RECURSO);
+            peliculas.forEach((pelicula) -> {
+                System.out.println("pelicula = " + pelicula);
+            });
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
-    public void iniciarArchivo(String nombreArchivo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void buscarPelicula(String buscar) {
+        String resultado = null;
+        try {
+            resultado = this.datos.buscar(NOMBRE_RECURSO, buscar);
+            
+        }catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
+        System.out.println("resultado = " + resultado);
     }
-    
+
+    @Override
+    public void iniciarCatalogoPeliculas() {
+        try {
+            if(this.datos.existe(NOMBRE_RECURSO))
+            {
+                datos.borrar(NOMBRE_RECURSO);
+                datos.crear(NOMBRE_RECURSO);
+            }
+            else
+            {
+                datos.crear(NOMBRE_RECURSO);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace(System.out);
+        }
+    }
+
 }
